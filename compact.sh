30b3d6c980295427973ee7ff03a3c132491e79ef
Benjamin SANS <bsans@capensis.fr>
2013-05-02 22:43:41 +0200

    add: compacter to generate a self extractible archive

---
+++ compact.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tar cvzf package.tgz main.js VERSION js/
+
+var=$(base64 <package.tgz)
+
+nvar=$(echo "$var" | perl -pe "s/\n/#NEWLINE#/g")
+
+rm package.tgz
+
+perl -pe "s|^package=.*$|package='$nvar'|" zerocli.sh >zerocli.tmp
+perl -pe "s|#NEWLINE#|\n|g" zerocli.tmp >zerocli.tmp.tmp
+
+mv zerocli.tmp.tmp zerocli
+rm zerocli.tmp
